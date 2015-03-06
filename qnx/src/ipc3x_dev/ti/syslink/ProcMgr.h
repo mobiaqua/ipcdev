e05803341b866731fcf7aa3d64ada2428b90a4f0
vwan@ti.com <vwan@ti.com>
2015-03-05 17:54:09 -0800

    Increase default value ProcMgr_MAX_MEMORY_REGIONS in QNX
    This commit increases the default value of ProcMgr_MAX_MEMORY_REGIONS to
    match what we used to support in SysLink 2 (SDOCM00115108), so that real
    applications that require many ProcMgr address mappings do not easily
    run into this limit.
    
    Signed-off-by: VW <vwan@ti.com>

--- qnx/src/ipc3x_dev/ti/syslink/ProcMgr.h
+++ qnx/src/ipc3x_dev/ti/syslink/ProcMgr.h
@@ -41,7 +41,7 @@
  *
  *  ============================================================================
  *
- *  Copyright (c) 2008-2009, Texas Instruments Incorporated
+ *  Copyright (c) 2008-2015, Texas Instruments Incorporated
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -238,7 +238,7 @@ extern "C" {
 /*!
  *  @brief  Maximum number of memory regions supported by ProcMgr module.
  */
-#define ProcMgr_MAX_MEMORY_REGIONS  32u
+#define ProcMgr_MAX_MEMORY_REGIONS  96u
 
 /*!
  *  @def    IS_VALID_PROCID
