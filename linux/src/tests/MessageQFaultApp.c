ac4579706306d1976521a8dda302678536ae21d3
Sam Nelson <sam.nelson@ti.com>
2017-03-31 09:41:41 -0400

    fault: Linux: Renamed test application fault to MessageQFaultApp
    Updated Makefile.am accordingly
    
    Signed-off-by: Sam Nelson <sam.nelson@ti.com>

--- linux/src/tests/MessageQFaultApp.c
+++ linux/src/tests/MessageQFaultApp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2016 Texas Instruments Incorporated - http://www.ti.com
+ * Copyright (c) 2012-2017 Texas Instruments Incorporated - http://www.ti.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
